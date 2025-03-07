c62d29a78e771a82468dcd8e53a0886c1ad54725
Erick Marcondes de Mattos Carpes <erickcarpesmmc@gmail.com>
2025-03-06 21:50:11 -0300

    Update AboutMe.tsx
    changed my age to 19

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -7,7 +7,7 @@ export function AboutMe(){
 
                 <Box>
                     <Heading id={"aboutMe"} fontSize={"25px"} fontFamily={"candara"} fontWeight={"bold"}>Who am I?</Heading>
-                    <Text mt="15px" textAlign={"justify"} fontFamily={"candara light"} fontSize={"17px"}>Hello, I’m Erick Carpes, an 18-year-old Software Engineering student with a passion for technology and a background in competitive sailing. My journey as an athlete since the age of 8 has instilled in me key qualities like discipline, resilience, and dedication: traits that I carry into my approach to software development. </Text> 
+                    <Text mt="15px" textAlign={"justify"} fontFamily={"candara light"} fontSize={"17px"}>Hello, I’m Erick Carpes, an 19-year-old Software Engineering student with a passion for technology and a background in competitive sailing. My journey as an athlete since the age of 8 has instilled in me key qualities like discipline, resilience, and dedication: traits that I carry into my approach to software development. </Text> 
                     
                     <Text mt={"15px"} textAlign={"justify"} fontFamily={"candara light"} fontSize={"17px"}>While still early in my academic career, I’ve already begun improving my skills in various programming languages and frameworks, consistently pushing myself to learn more. I’m particularly motivated by challenges that allow me to combine logic, creativity, and innovation to build effective solutions. Whether it’s a complex algorithm or an intuitive user interface, I bring a mindset of continuous improvement and adaptability to everything I do. </Text>
                     
